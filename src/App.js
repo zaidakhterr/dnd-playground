6215133ca9bfefa02b6d6d8a6426fae8aeb1df18
Zaid Akhter <zaidakhter1202@gmail.com>
2020-06-14 23:48:08 +0500

    👌 IMPROVE: Hide source(card) on drag

--- src/App.js
+++ src/App.js
@@ -7,30 +7,59 @@ import { ItemTypes } from "./ItemTypes";
 const App = () => {
   return (
     <div className="app">
-      <Card text="Card 1" />
+      <Container />
     </div>
   );
 };
 
-const Card = ({ text }) => {
-  const [, drag] = useDrag({ item: { type: ItemTypes.CARD } });
+const Container = ({}) => {
+  return (
+    <div
+      style={{
+        width: 800,
+        height: 500,
+        margin: 20,
+        border: "2px solid #222",
+        borderRadius: 3,
+        position: "relative",
+      }}
+    >
+      <Card top={10} left={10} text="Card 1" hideSourceOnDrag={true} />
+    </div>
+  );
+};
+
+const Card = ({ top, left, text, hideSourceOnDrag }) => {
+  const [{ isDragging }, drag] = useDrag({
+    item: { type: ItemTypes.CARD },
+    collect: (monitor) => ({
+      isDragging: monitor.isDragging(),
+    }),
+  });
+
+  if (isDragging && hideSourceOnDrag) {
+    return <div ref={drag} />;
+  }
+
   return (
     <div
       ref={drag}
       className="card"
       style={{
         width: 200,
-        height: 300,
+        height: 100,
         textAlign: "center",
         textTransform: "uppercase",
         padding: 20,
-        margin: 20,
         backgroundColor: "#f5f5f5",
         border: "1px solid #d9d9d9",
         borderRadius: 5,
         display: "flex",
         alignItems: "center",
         justifyContent: "center",
+        position: "absolute",
+        top,
+        left,
       }}
     >
       <h3>{text}</h3>
