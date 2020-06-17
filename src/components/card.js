2695e863b884bd7316622c20bfe81c88eab211bc
Zaid Akhter <zaidakhter1202@gmail.com>
2020-06-17 21:11:57 +0500

    👌 IMPROVE: Edit card text on the go

--- src/components/card.js
+++ src/components/card.js
@@ -1,9 +1,12 @@
-import React, { memo } from "react";
+import React, { memo, useState } from "react";
 import { useDrag } from "react-dnd";
 
 import { ItemTypes } from "../ItemTypes";
 
-const Card = ({ id, top, left, width, height, text }) => {
+const Card = ({ id, top, left, width, height, text: initialText }) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [text, setText] = useState(initialText);
+
   const [{ isDragging }, drag] = useDrag({
     item: { id, left, top, type: ItemTypes.CARD },
     collect: (monitor) => ({
@@ -25,8 +28,25 @@ const Card = ({ id, top, left, width, height, text }) => {
         top,
         left,
       }}
+      onDoubleClick={() => {
+        setIsEditing(true);
+      }}
+      onMouseLeave={() => {
+        setIsEditing(false);
+      }}
     >
-      <h4>{text}</h4>
+      {isEditing ? (
+        <input
+          value={text}
+          type="text"
+          autoFocus
+          onChange={(e) => {
+            setText(e.target.value);
+          }}
+        />
+      ) : (
+        <p>{text}</p>
+      )}
     </div>
   );
 };
