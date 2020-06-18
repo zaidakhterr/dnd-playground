a28ddc4967d5510539ce30e032c2d8be1d03ed99
Zaid Akhter <zaidakhter1202@gmail.com>
2020-06-18 12:19:54 +0500

    📦 NEW: Add draggable rings

---
+++ src/components/ring.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { useDrag } from "react-dnd";
+
+import { ItemTypes } from "../ItemTypes";
+
+const Ring = ({ id }) => {
+  const [{ isDragging }, drag] = useDrag({
+    item: { id, type: ItemTypes.RING },
+    collect: (monitor) => ({
+      isDragging: monitor.isDragging(),
+    }),
+  });
+
+  if (isDragging) {
+    return <div ref={drag} />;
+  }
+
+  return (
+    <div
+      ref={drag}
+      className="ring"
+      style={{
+        width: 100 * id,
+        backgroundColor: id === 1 ? "56a8eb" : id === 2 ? "#eb56be" : "#86e46f",
+      }}
+    >
+      {id}
+    </div>
+  );
+};
+
+export default Ring;
--- src/pages/towerofhanoi.js
+++ src/pages/towerofhanoi.js
@@ -1,9 +1,15 @@
 import React from "react";
+import Ring from "../components/ring";
 
 const TowerOfHanoi = () => {
   return (
     <div className="tower-of-hanoi">
       <h1>Tower of Hanoi</h1>
+      <div className="tower">
+        <Ring id={1} />
+        <Ring id={2} />
+        <Ring id={3} />
+      </div>
     </div>
   );
 };
