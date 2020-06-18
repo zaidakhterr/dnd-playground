f80f84048b8684818e15eebe04ed29ddcc43f04a
Zaid Akhter <zaidakhter1202@gmail.com>
2020-06-18 19:17:10 +0500

    📦 NEW: Add Restart and Play Again buttons

--- src/pages/towerofhanoi.js
+++ src/pages/towerofhanoi.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useRecoilValue } from "recoil";
+import { useRecoilValue, useResetRecoilState } from "recoil";
 
 import { towerOfHanoi, towerOfHanoiMoves } from "../atoms";
 import Tower from "../components/tower";
@@ -9,7 +9,15 @@ const TowerOfHanoi = () => {
   const towers = useRecoilValue(towerOfHanoi);
   const moves = useRecoilValue(towerOfHanoiMoves);
 
+  const resetTowers = useResetRecoilState(towerOfHanoi);
+  const resetMoves = useResetRecoilState(towerOfHanoiMoves);
+
   const message = moves === 7 ? "Perfect Score." : moves > 7 && moves < 14 ? "Good Score." : "But you can do better.";
+
+  const handleReplay = () => {
+    resetMoves();
+    resetTowers();
+  };
 
   return (
     <div className="tower-of-hanoi">
@@ -21,7 +29,21 @@ const TowerOfHanoi = () => {
           <Tower key={key} id={key} />
         ))}
       </div>
-      {towers["3"].length === 3 && <div className="win">Yayy! You Won. {message}</div>}
+      {towers["3"].length === 3 && (
+        <div className="win">
+          <p>Yayy! You Won. {message}</p>
+          <button className="lg" onClick={handleReplay}>
+            Play Again
+          </button>
+        </div>
+      )}
+      {moves > 0 && towers["3"].length !== 3 && (
+        <div className="win">
+          <button className="lg" onClick={handleReplay}>
+            Restart
+          </button>
+        </div>
+      )}
     </div>
   );
 };
